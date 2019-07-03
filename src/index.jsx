fe992b1cd3af7219ff50c58961a82e7429259900
Saúl Hormazábal <saul.hormazabal@gmail.com>
2019-07-02 23:45:06 -0400

    Added react-router-dom

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Landing from './scenes/Landing';
-import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Landing from './scenes/Landing';
+import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Landing />, document.getElementById('root'));
+
+const Router = () => (
+  <BrowserRouter>
+    <Route exact path="/" component={Landing} />
+  </BrowserRouter>
+)
+
+
+ReactDOM.render(<Router />, document.getElementById('root'));
 
 
 serviceWorker.register();
